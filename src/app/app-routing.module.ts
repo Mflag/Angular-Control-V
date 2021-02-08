7eef270e646136d43367aaf9b87fa763c305710d
Mflag <matiibandera@outlook.com>
2021-02-08 16:17:11 -0300

    Modificaiones a la interfas de logueo, registro y buscador

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,30 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProductsComponent } from 'src/components/addProducts/addProducts.component';
 import { CatalogoComponent } from 'src/components/catalogo/catalogo.component';
+import { LoginComponent } from 'src/components/login/login.component';
 import { MainComponent } from 'src/components/main/main.component';
+import { SingUpComponent } from 'src/components/singUp/singUp.component';
 
 const routes: Routes = [
   {
-    path: 'main', component: MainComponent
+    path: 'main', 
+    component: MainComponent
   },
   {
-    path: 'addProducts', component:AddProductsComponent
+    path: 'addProducts', 
+    component:AddProductsComponent
   } ,
   {
-    path: 'catalogo' , component: CatalogoComponent
+    path: 'catalogo' , 
+    component: CatalogoComponent
+  },
+  {
+    path: 'login', 
+    component: LoginComponent
+  },
+  {
+    path: 'singUp', 
+    component: SingUpComponent    
   },
   {
     path: '**', redirectTo: 'main'
