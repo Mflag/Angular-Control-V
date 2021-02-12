815904d135c69e9549a60b5e52c543ca18294106
Mflag <matiibandera@outlook.com>
2021-02-12 18:29:09 -0300

    capturar datos del formulario

--- src/components/singUp/singUp.component.ts
+++ src/components/singUp/singUp.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingUpComponent implements OnInit {
  user={
-   email:'',
-   password:'' 
+    nombre:'',
+    email:'',
+    telefono:'',
+    password:'',
+    confirmacion:'', 
  }
 
   constructor() { }
